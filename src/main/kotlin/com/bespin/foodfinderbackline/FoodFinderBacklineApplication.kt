6b78a88bb247b2446e7faf49ddc794d937da0857
Will Madison <wmadison@Wills-MacBook-Pro.local>
2019-02-27 14:20:51 -0500

    initial commit

---
+++ src/main/kotlin/com/bespin/foodfinderbackline/FoodFinderBacklineApplication.kt
@@ -0,0 +1,11 @@
+package com.bespin.foodfinderbackline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodFinderBacklineApplication
+
+fun main(args: Array<String>) {
+    runApplication<FoodFinderBacklineApplication>(*args)
+}
